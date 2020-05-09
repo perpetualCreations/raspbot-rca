733ab203a763159139133725ed9420d476ef9aa1
tchen <tchen0584@gmail.com>
2020-05-09 11:46:24 -0400

    Test script for RAKMODULE.

---
+++ test.ino
@@ -0,0 +1,35 @@
+// Raspbot Remote Control Application (Raspbot RCA-G), Arduino Instructions for Serial Commandment
+// RAKMODULE Test Sketch
+
+// Based off of:
+/* Sweep
+ by BARRAGAN <http://barraganstudio.com>
+ This example code is in the public domain.
+
+ modified 8 Nov 2013
+ by Scott Fitzgerald
+ http://www.arduino.cc/en/Tutorial/Sweep
+*/
+
+#include <Servo.h>
+
+Servo RAK1SERVO;
+Servo RAK2SERVO;
+
+int pos = 0;    // variable to store the servo position
+
+void setup() {
+  myservo.attach(10);  // attaches the servo on pin 9 to the servo object
+}
+
+void loop() {
+  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+}
