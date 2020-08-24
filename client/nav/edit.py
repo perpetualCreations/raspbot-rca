d0881c9d0a27f323a6ffbbbeaaba7806f3a2be3b
tchen <tchen0584@gmail.com>
2020-08-24 16:00:42 -0400

    Modularization complete.

--- client/nav/edit.py
+++ client/nav/edit.py
@@ -6,6 +6,7 @@ Made by Taian Chen
 Navigation script editing interface.
 """
 
+from nav import objects
 
 def nav_edit():
     """
@@ -13,34 +14,34 @@ def nav_edit():
     Also similarly to client.set_configuration_gui() will be updated to an actual editor.
     :return: none.
     """
-    platform = system()
+    platform = objects.system()
     if platform in ["Linux", "Ubuntu", "Debian", "Raspbian"]:
-        root = tkinter.Toplevel()
+        root = objects.tkinter.Toplevel()
         root.title("Raspbot RCA: Nav Name Entry")
         root.configure(bg = "#344561")
         root.geometry('{}x{}'.format(260, 131))
         root.resizable(width = False, height = False)
-        graphics_title = tkinter.Label(root, text = "Enter script name.", fg = "white", bg = "#344561", font = ("Calibri", 16))
+        graphics_title = objects.tkinter.Label(root, text = "Enter script name.", fg = "white", bg = "#344561", font = ("Calibri", 16))
         graphics_title.grid(row = 0, column = 0)
-        graphics_entry = tkinter.Entry(root, bg = "white", fg = "black", font = ("Calibri", 12))
+        graphics_entry = objects.tkinter.Entry(root, bg = "white", fg = "black", font = ("Calibri", 12))
         graphics_entry.grid(row = 1, column = 0, padx = (10, 0))
-        graphics_confirm_button = tkinter.Button(root, bg = "white", fg = "black", text = "Confirm", width = 8, height = 1, font = ("Calibri", 12), command = lambda: call("sudo nano " + graphics_entry.get(), shell = True))
+        graphics_confirm_button = objects.tkinter.Button(root, bg = "white", fg = "black", text = "Confirm", width = 8, height = 1, font = ("Calibri", 12), command = lambda: call("sudo nano " + graphics_entry.get(), shell = True))
         graphics_confirm_button.grid(row = 2, column = 0, padx = (10, 0), pady = (5, 0))
         root.mainloop()
     elif platform == "Windows":
-        root = tkinter.Toplevel()
+        root = objects.tkinter.Toplevel()
         root.title("Raspbot RCA: Nav Name Entry")
         root.configure(bg = "#344561")
         root.geometry('{}x{}'.format(260, 131))
         root.resizable(width = False, height = False)
-        graphics_title = tkinter.Label(root, text = "Enter script name.", fg = "white", bg = "#344561", font = ("Calibri", 16))
+        graphics_title = objects.tkinter.Label(root, text = "Enter script name.", fg = "white", bg = "#344561", font = ("Calibri", 16))
         graphics_title.grid(row = 0, column = 0)
-        graphics_entry = tkinter.Entry(root, bg = "white", fg = "black", font = ("Calibri", 12))
+        graphics_entry = objects.tkinter.Entry(root, bg = "white", fg = "black", font = ("Calibri", 12))
         graphics_entry.grid(row = 1, column = 0, padx = (10, 0))
-        graphics_confirm_button = tkinter.Button(root, bg = "white", fg = "black", text = "Confirm", width = 8, height = 1, font = ("Calibri", 12), command = lambda: Popen(["notepad.exe", graphics_entry.get()]))
+        graphics_confirm_button = objects.tkinter.Button(root, bg = "white", fg = "black", text = "Confirm", width = 8, height = 1, font = ("Calibri", 12), command = lambda: Popen(["notepad.exe", graphics_entry.get()]))
         graphics_confirm_button.grid(row = 2, column = 0, padx = (10, 0), pady = (5, 0))
         root.mainloop()
     else:
-        messagebox.showerror("Raspbot RCA: OS Unsupported", "Client OS is unsupported, please manually edit configuration! Open an issue on Github for your operating system to be supported.")
+        objects.messagebox.showerror("Raspbot RCA: OS Unsupported", "Client OS is unsupported, please manually edit configuration! Open an issue on Github for your operating system to be supported.")
     pass
 pass
