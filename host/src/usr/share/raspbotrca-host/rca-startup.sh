64c9bfd54b2c6ff4e52388472aea0276bab6e8bb
tchen <tchen0584@gmail.com>
2021-03-08 10:59:38 -0500

    Patched /etc/init.d script.

--- host/src/usr/share/raspbotrca-host/rca-startup.sh
+++ host/src/usr/share/raspbotrca-host/rca-startup.sh
@@ -1,2 +1,17 @@
-#!/bin/sh
-sudo raspbotrca-host-manualstart
+#!/bin/bash
+# description: Raspbot RCA host application service.
+
+case "$1" in
+    start)
+       sudo python3 /usr/bin/raspbotrca-host-manualstart
+       ;;
+    stop)
+       sudo pkill -9 -f /usr/share/raspbotrca-host/main.py
+       ;;
+    restart)
+       sudo pkill -9 -f /usr/share/raspbotrca-host/main.py
+       sudo python3 /usr/bin/raspbotrca-host-manualstart
+       ;;
+esac
+
+exit 0
